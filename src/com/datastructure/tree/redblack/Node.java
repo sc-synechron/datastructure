8eb1d23adc4caec21db3f93d9d11770ad8b709b7
Saurabh.Chaturvedi <Saurabh.Chaturvedi@.synechron.com>
2019-01-09 20:08:04 +0530

    test code

---
+++ src/com/datastructure/tree/redblack/Node.java
@@ -0,0 +1,13 @@
+package com.datastructure.tree.redblack;
+
+import java.math.BigInteger;
+
+public class Node<T extends Comparable<T>> {
+
+	T data;
+	Node leftChild;
+	Node rightChild;
+	
+	///BigInteger
+
+}
---
+++ src/com/datastructure/tree/redblack/Tree.java
@@ -0,0 +1,5 @@
+package com.datastructure.tree.redblack;
+
+public class Tree {
+
+}
